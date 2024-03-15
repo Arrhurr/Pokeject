0499d6dce847557668957263f81d519146e45ca0
Pierre Hamon <pierrehmn1@gmail.com>
2024-03-15 17:11:07 +0100

    finis les fonctions test jusqu'à la 4

---
+++ test.js
@@ -0,0 +1,38 @@
+function getPokemonsByType(typeName){
+    var types_found =[]
+    if(Type.exist(typeName)==0){
+        for(var i=0;i<Pokemon.all_pokemons.length;i++){
+            for(var y=0;y<Pokemon.all_pokemons[i].type.length;y++){    
+                if(Pokemon.all_pokemons[i].type[y].type==typeName){
+                    types_found.push(Pokemon.all_pokemons[i]);
+                }  
+            }
+        }  
+    }
+    else{
+        console.log("Erreur le type entré est invalide")
+    }
+   
+    return types_found
+}
+
+
+function getPokemonsByAttack(attackName){
+    var attack_found= [];
+        for(var i=0;i<Pokemon.all_pokemons.length;i++){
+            for(var y=0;y<Pokemon.all_pokemons[i].attaque.length;y++){
+                if(Pokemon.all_pokemons[i].attaque[y].att_name==attackName){
+                    attack_found.push(Pokemon.all_pokemons[i])
+                }
+            }
+        }
+    return attack_found
+}
+
+function sortPokemonByName(){
+    var liste_noms= []
+    for(var i=0;i<Pokemon.all_pokemons.length;i++){
+        liste_noms.push(Pokemon.all_pokemons[i].nom)
+    }
+    return liste_noms.sort()
+}
